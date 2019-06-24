b8a581237184dc3c2e2190905ad85430b5d39c0d
Viet <nguyenviet.vn.07@gmail.com>
2019-06-24 09:31:10 +0700

    first ommit

---
+++ hinhcn.js
@@ -0,0 +1,8 @@
+let sodai
+let sorong
+sodai = prompt("nhap so dai")
+sorong = prompt("nhap so rong")
+let rong = parseInt(sorong)
+let dai = parseInt(sodai)
+let area = dai * rong
+document.write("dientich = " +area )
